3a1bd6e0b6e63ef58b83ac467e27f7741142d1bf
Upendra Gusain <upendra.gusain@gmail.com>
2020-07-06 14:03:45 +0100

    never allow your domain classes to be in an invalid state

--- App/Student.cs
+++ App/Student.cs
@@ -6,9 +6,20 @@ namespace App
 {
     public class Student
     {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public long FavoriteCourseId { get; set; }
+        //EF core uses refection to assign values to the backing fields for properties
+        // hence you can use private setters
+        // you can also entirely remove setters
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public long FavoriteCourseId { get; }
+
+        public Student(string name, 
+            string email, long favoriteCourseId)
+        {
+            Name = name;
+            Email = email;
+            FavoriteCourseId = favoriteCourseId;
+        }
     }
 }
